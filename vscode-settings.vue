48f88dc13f9bafeea945c41ac1f4b6e8af3a8a85
odinnmk <39589991+odinnmk@users.noreply.github.com>
2018-05-24 11:59:42 +0000

    Add files via upload

---
+++ vscode-settings.vue
@@ -0,0 +1,8 @@
+{
+    "workbench.colorTheme": "Monokai",
+    "workbench.sideBar.location": "left",
+    "workbench.statusBar.visible": true,
+    "workbench.activityBar.visible": false,
+    "workbench.editor.showTabs": false,
+    "workbench.iconTheme": "Monokai Classic Icons"
+}
